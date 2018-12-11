3170f7d5c7b322394d375fe24a871f4e3cf604ce
jike <jike@kezaihui.com>
2018-12-11 15:52:19 +0800

    update test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -10,16 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:install_plugin_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
+  testWidgets('Verify Widgets', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-        find.byWidgetPredicate(
-          (Widget widget) =>
-              widget is Text && widget.data.startsWith('Running on:'),
-        ),
-        findsOneWidget);
+    expect(find.byType(FlatButton), findsNWidgets(2));
   });
 }
