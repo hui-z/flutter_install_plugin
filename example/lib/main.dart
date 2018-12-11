b5e29d77e62a8f468bc58d142394521d82557e16
jike <jike@kezaihui.com>
2018-12-11 14:34:33 +0800

    add permission handler for example; fix android run fail

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:install_plugin/install_plugin.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(new MyApp());
 
@@ -14,6 +15,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   String _appUrl = '';
+  String _apkFilePath = '';
 
   @override
   void initState() {
@@ -45,6 +47,12 @@ class _MyAppState extends State<MyApp> {
         body: new Column(
           children: <Widget>[
             Text(_platformVersion),
+            TextField(
+              decoration: InputDecoration(
+                  hintText:
+                      'apk file path to install. Like /storage/emulated/0/demo/update.apk'),
+              onChanged: (path) => _apkFilePath = path,
+            ),
             FlatButton(
                 onPressed: () {
                   onClickInstallApk();
@@ -64,14 +72,23 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  void onClickInstallApk() {
-    InstallPlugin.installApk('/storage/emulated/0/zpartner/update.apk',
-            'com.zaihui.installpluginexample')
-        .then((result) {
-      print('install apk $result');
-    }).catchError((error) {
-      print('install apk error: $error');
-    });
+  void onClickInstallApk() async {
+    if (_apkFilePath.isEmpty) {
+      print('make sure the apk file is set');
+      return;
+    }
+    Map<PermissionGroup, PermissionStatus> permissions =
+        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
+      InstallPlugin.installApk(_apkFilePath, 'com.zaihui.installpluginexample')
+          .then((result) {
+        print('install apk $result');
+      }).catchError((error) {
+        print('install apk error: $error');
+      });
+    } else {
+      print('Permission request fail!');
+    }
   }
 
   void onClickGotoAppStore(String url) {
