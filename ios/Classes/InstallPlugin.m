bd0144f4dc2c835c82aca392aafcf2d73eddabf7
StevieYu <stevieyu@users.noreply.github.com>
2019-07-22 17:14:03 +0800

    Update InstallPlugin.m

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -1,5 +1,5 @@
 #import "InstallPlugin.h"
-//#import <install_plugin/install_plugin-Swift.h>
+#import <install_plugin/install_plugin-Swift.h>
 
 @implementation InstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
